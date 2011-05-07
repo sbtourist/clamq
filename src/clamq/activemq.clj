1bc8a16bd6f0cc0d52f1630c165bc37e4052f9fe
Sergio Bossa <sergio.bossa@gmail.com>
2011-05-07 12:08:39 +0200

    Renamed activemq-connection function to simply activemq.

--- src/clamq/activemq.clj
+++ src/clamq/activemq.clj
@@ -5,7 +5,7 @@
    )
  )
 
-(defn activemq-connection [broker & {username :username password :password max-connections :max-connections :or {max-connections 1}}]
+(defn activemq [broker & {username :username password :password max-connections :max-connections :or {max-connections 1}}]
   "Returns an ActiveMQ javax.jms.ConnectionFactory pointing to the given broker url.
 It currently supports the following optional named arguments (refer to ActiveMQ docs for more details about them):
 :username, :password, :max-connections."
