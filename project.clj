b7726408a400cbcb1d15b51a25715f2b28165fb9
Sergio Bossa <sergio.bossa@gmail.com>
2011-08-19 19:03:10 +0200

    Removed unused dep.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
  :description "Clojure APIs for Message Queues"
  :dependencies [
                 [org.clojure/clojure "1.2.1"]
-                [org.clojure/clojure-contrib "1.2.0"]
 
                 [org.springframework/spring-context "3.0.5.RELEASE"]
                 [org.springframework/spring-jms "3.0.5.RELEASE"]
