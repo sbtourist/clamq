7a9c79e9879bf8e55796a404b26e895570ed2683
Sergio Bossa <sergio.bossa@gmail.com>
2011-12-28 16:28:15 +0100

    Improved runner to automatically require all namespaces.

--- clamq-runner/src/clamq/runner/main.clj
+++ clamq-runner/src/clamq/runner/main.clj
@@ -4,7 +4,9 @@
    [clamq.protocol.consumer :as consumer]
    [clamq.protocol.seqable :as seqable]
    [clamq.protocol.producer :as producer]
-   [clamq.protocol.pipe :as pipe])
+   [clamq.pipes :as pipes]
+   [clamq.activemq :as activemq]
+   [clamq.rabbitmq :as rabbitmq])
  (:gen-class))
 
 (defn -main [& args]
