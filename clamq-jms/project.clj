7ce88cc81f469decbcb1ac67b580cf3f6ccc6361
Glenn Phillips <gphillips@carelogistics.com>
2016-08-22 13:37:00 -0400

    Added flexible key formatter for the key's in the JMS Property Header

--- clamq-jms/project.clj
+++ clamq-jms/project.clj
@@ -2,4 +2,5 @@
  :description "Clojure APIs for Message Queues"
  :dependencies [[clamq/clamq-core "0.5-SNAPSHOT"]
                 [org.slf4j/slf4j-api "1.7.21"]
-                [org.springframework/spring-jms "4.3.2.RELEASE"]])
+                [org.springframework/spring-jms "4.3.2.RELEASE"] 
+                [camel-snake-kebab "0.4.0"]])
--- clamq-jms/src/clamq/converters.clj
+++ clamq-jms/src/clamq/converters.clj
@@ -1,4 +1,5 @@
 (ns clamq.converters
+  (:require [camel-snake-kebab.core :refer [->PascalCase]])
   (:import [org.springframework.jms.support.converter MessageConverter SimpleMessageConverter]
            [org.springframework.jms.core JmsTemplate MessageCreator] 
            [javax.jms Session TextMessage DeliveryMode]))
@@ -26,6 +27,7 @@
   (.setJMSExpiration jms-message (:expiration headers 0))
   (.setJMSPriority jms-message (:priority headers 4))
   (.setJMSRedelivered jms-message (:redelivered headers false))
+  (.setJMSType jms-message (:jms-type headers))
   ;; (.setJMSReplyTo jms-message (:reply-to headers))
   jms-message)
 
@@ -35,9 +37,10 @@
      (map #(keyword %) (keys properties))
      (vals properties))))
 
-(defn- properties->jms-message [jms-message properties]
+(defn- properties->jms-message [jms-message properties {:keys [key-fn]
+                                                        :or   {key-fn ->PascalCase}}]
   (doseq [[k v] properties] 
-    (.setStringProperty jms-message (name k) v))
+    (.setStringProperty jms-message (key-fn (name k)) v))
   jms-message)
 
 (defn body-and-header-converter []
@@ -52,27 +55,16 @@
     (toMessage [this message session]
       (.toMessage (SimpleMessageConverter.) message session))))
 
-(defn- text-message [session message headers]
+(defn- text-message [session message headers opts]
   (let [txt-msg (-> (.createTextMessage session message)
                     (headers->jms-message headers)
-                    (properties->jms-message (:properties headers)))]
-    (prn "TextMessage: " txt-msg)
+                    (properties->jms-message (:properties headers) opts))]
     txt-msg))
 
 (defn message-creator 
   "Creates an implementation of a Spring MessageCreator. This implementation creates a
    TextMessage and sets any provided headers and properties on that message." 
-  [message headers]
+  [message headers & [opts]]
   (reify MessageCreator
     (createMessage [_ session]
-      (text-message session message headers))))
-    
-;; (defn proxy-message-post-processor 
-;;   "Provides implementation of Spring's MessagePostProcessor. Used to set the 
-;;    headers (and properties) on a message before it is sent"
-;;   [{properties :properties :as headers}]
-;;   (reify MessagePostProcessor 
-;;     (postProcessMessage [_ message]
-;;       (-> message
-;;           (map->message-headers headers)
-;;           (map->message-properties headers)))))
+      (text-message session message headers opts))))
