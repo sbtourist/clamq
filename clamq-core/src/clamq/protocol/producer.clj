48cb7facc20667c80c58bf27b5b142e8b3b75bb4
Glenn Phillips <gphillips@carelogistics.com>
2016-08-22 13:37:38 -0400

    removed the extra arity on request-reply

--- clamq-core/src/clamq/protocol/producer.clj
+++ clamq-core/src/clamq/protocol/producer.clj
@@ -2,4 +2,4 @@
 
 (defprotocol Producer
   (publish [self destination message] [self destination message attributes]) 
-  (request-reply [self destination message][self destination message attributes]))
+  (request-reply [self destination message attributes]))
